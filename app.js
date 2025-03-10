a1a60f7c8a7257a125102d86cebff6b717e070d4
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-10 11:36:21 +0000

    add in functionality for making requests to non existent paths

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const endpoints = require("./endpoints.json");
-const {getTopics} = require("./controllers/topics.controllers")
+const { getTopics } = require("./controllers/topics.controllers");
 
 app.get("/api", (request, response) => {
-  response.status(200).send({endpoints});
+  response.status(200).send({ endpoints });
 });
 
-app.get("/api/topics", getTopics)
+app.get("/api/topics", getTopics);
 
-
+app.all("*", (request, response, next) => {
+  response.status(404).send({ msg: "path not found" });
+});
 
 module.exports = app;
