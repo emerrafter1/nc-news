d428213f571e49796f4e7dd5b4d99ce852152df6
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-10 16:25:51 +0000

    separate topics and articles controllers

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,21 +1,7 @@
-const {fetchTopics, fetchArticleById} = require("../models/topics.models")
+const { fetchTopics } = require("../models/topics.models");
 
-function getTopics(request, response){
-
-    fetchTopics().then((topics) =>
-    response.status(200).send({topics: topics}))
+function getTopics(request, response) {
+  fetchTopics().then((topics) => response.status(200).send({ topics: topics }));
 }
 
-function getArticleById(request, response, next){
-
-    const articleId = request.params.article_id
-    
-
-    fetchArticleById(articleId).then((article) => {
-        response.status(200).send({article: article})
-    }).catch((error) => {
-        next(error)
-    })
-}
-
-module.exports = {getTopics, getArticleById}
+module.exports = { getTopics };
