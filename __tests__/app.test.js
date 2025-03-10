300f3f3ef98b7e661b3ec2b33236d9ca2c2077c7
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-10 11:28:50 +0000

    update test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,16 @@
 const endpointsJson = require("../endpoints.json");
 const app = require("../app");
 const request = require("supertest");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
+
+beforeEach(() => seed(data));
+
+afterAll(() => db.end());
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -12,6 +19,22 @@ describe("GET /api", () => {
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an object detailing all the topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const topics = body.topics;
+        expect(topics.length).toBe(3);
+        topics.forEach((topic) => {
+          expect(typeof topic.description).toBe("string");
+          expect(typeof topic.slug).toBe("string");
+        });
       });
   });
 });
