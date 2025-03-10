fd9ae27ac4cdceb6484dfe894f52a776db84840b
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-10 16:24:02 +0000

    add articles models

---
+++ models/articles.models.js
@@ -0,0 +1,27 @@
+const db = require("../db/connection");
+
+function fetchArticles() {
+  return db
+    .query(
+      `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comments.comment_id) AS INT) AS comment_count FROM articles FULL JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC;`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+}
+
+function fetchArticleById(articleId) {
+  return db
+    .query(
+      `SELECT author, title, article_id, body, topic, created_at, votes, article_img_url FROM articles WHERE article_id = $1;`,
+      [articleId]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found" });
+      }
+      return rows[0];
+    });
+}
+
+module.exports = { fetchArticles, fetchArticleById };
