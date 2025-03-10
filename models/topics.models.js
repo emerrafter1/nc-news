f790004ed348f8ba1e76ee440c8c7634c3f02065
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-10 14:05:25 +0000

    update topics models to include get article by id

--- models/topics.models.js
+++ models/topics.models.js
@@ -6,4 +6,18 @@ function fetchTopics() {
   });
 }
 
-module.exports = { fetchTopics };
+function fetchArticleById(articleId) {
+  return db
+    .query(
+      `SELECT author, title, article_id, body, topic, created_at, votes, article_img_url FROM articles WHERE article_id = $1;`,
+      [articleId]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found" });
+      }
+      return rows[0];
+    });
+}
+
+module.exports = { fetchTopics, fetchArticleById };
