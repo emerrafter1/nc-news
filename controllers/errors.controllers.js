f5402dba94c601c091f600ca417dcd17b0ee75c5
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-10 14:03:48 +0000

    add error controllers

--- controllers/errors.controllers.js
+++ controllers/errors.controllers.js
@@ -1,5 +1,19 @@
-function handleServerErrors(error, request, response, next) {
-    response.status(500).send({msg: "Server Error"})
+function handlePsqlError(error, request, response, next) {
+  if (error.code === "22P02") {
+    response.status(400).send({ msg: "Bad request" });
+  }
+  next(error);
 }
 
-module.exports = { handleServerErrors };
+function handleCustomErrors(error, request, response, next) {
+  if (error.status) {
+    response.status(error.status).send({ msg: error.msg });
+  }
+  next(error);
+}
+
+function handleServerErrors(error, request, response, next) {
+  response.status(500).send({ msg: "Server Error" });
+}
+
+module.exports = { handleServerErrors, handlePsqlError, handleCustomErrors };
