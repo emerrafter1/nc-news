27c83f5c8bc18dd9399aeb41d46b9c5fb76f6e27
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-12 09:38:51 +0000

    update 404 message in checkExists

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -20,7 +20,7 @@ exports.checkExists = (table, column, value) => {
   const query = format("SELECT * FROM %I WHERE %I = $1;", table, column);
   return db.query(query, [value]).then(({ rows }) => {
     if (rows.length === 0) {
-      return Promise.reject({ status: 404, msg: "Resource not found" });
+      return Promise.reject({ status: 404, msg: "Not found" });
     }
     return true;
   });
