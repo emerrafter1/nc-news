fdb5e1adba5fad35079e84a9f1abe500a1f783d9
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-12 14:51:14 +0000

    added query to get user by username

--- models/users.models.js
+++ models/users.models.js
@@ -6,5 +6,14 @@ function fetchUsers() {
   });
 }
 
+function fetchUserByUsername(username){
+    return db.query(`SELECT * from users where username = $1`, [username]).then(({rows}) => {
+        if (rows.length === 0){
+            return Promise.reject({status:404, msg: "Not found" })
+        }
+        return rows[0]
+    })
+}
 
-module.exports = { fetchUsers };
+
+module.exports = { fetchUsers, fetchUserByUsername };
