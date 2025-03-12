89473e3b7710b1d20a224fb1fe23b46ec1adfda1
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-12 14:50:25 +0000

    updated controller for get user by username

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,7 +1,16 @@
-const { fetchUsers } = require("../models/users.models");
+const { fetchUsers, fetchUserByUsername } = require("../models/users.models");
 
 function getUsers(request, response) {
   fetchUsers().then((users) => response.status(200).send({ users: users }));
 }
 
-module.exports = { getUsers };
+function getUserByUsername(request, response, next) {
+    const {username} = request.params
+  fetchUserByUsername(username)
+    .then((user) => response.status(200).send({ user: user }))
+    .catch((err) => {
+      next(err);
+    });
+}
+
+module.exports = { getUsers, getUserByUsername };
