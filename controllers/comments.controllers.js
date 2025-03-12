b090d1f81080e19a8b6ac1a478583c66660d4bd8
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-12 15:30:20 +0000

    update controller to include patch comments

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,9 +1,7 @@
-const { removeComment } = require("../models/comments.models");
+const { removeComment, updateCommentVotes } = require("../models/comments.models");
 
 function deleteComment(request, response, next) {
-  const {comment_id} = request.params;
-
-
+  const { comment_id } = request.params;
 
   removeComment(comment_id)
     .then(() => {
@@ -14,4 +12,18 @@ function deleteComment(request, response, next) {
     });
 }
 
-module.exports = { deleteComment };
+function patchCommentVotes(request, response, next) {
+  const { comment_id } = request.params;
+  const { inc_votes } = request.body;
+
+
+  updateCommentVotes(inc_votes, comment_id)
+    .then((comment) => {
+      response.status(200).send({ comment: comment });
+    })
+    .catch((error) => {
+      next(error);
+    });
+}
+
+module.exports = { deleteComment, patchCommentVotes };
