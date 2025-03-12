7e3ed6f2b6bd9cf28880f758bdcd017927817857
emerrafter1 <emerlouiserafter@gmail.com>
2025-03-12 09:37:52 +0000

    update controller to include topic query

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -9,10 +9,16 @@ const {
 const { checkExists } = require("../db/seeds/utils");
 
 function getArticles(request, response, next) {
-  const { sort_by, order } = request.query;
+  const { sort_by, order, topic } = request.query;
 
-  fetchArticles(sort_by, order)
-    .then((articles) => {
+  promises = [fetchArticles(sort_by, order, topic)];
+
+  if (topic) {
+    promises.push(checkExists("topics", "slug", topic));
+  }
+
+  Promise.all(promises)
+    .then(([articles]) => {
       response.status(200).send({ articles: articles });
     })
     .catch((error) => {
